22b12ee3f95d27204dc861b160aaec615062cc42
Fredrik Hansen <fredrik.hansen@eve.sx>
2024-10-03 08:58:59 +0200

    work directory changed to /tmp

--- trmcodegen.py
+++ trmcodegen.py
@@ -33,5 +33,5 @@ config_list = [{
   }]
 
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
-user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding", "use_docker": False})
+user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "/tmp/trmcodegen/", "use_docker": False})
 user_proxy.initiate_chat(assistant, message=' '.join(argv[1:]))
